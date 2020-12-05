e72369023815d9f38a8060f4d2f9acfd0473359e
Danilocg07 <danilo-c-g@hotmail.com>
2020-12-05 15:44:34 -0300

    Primeiro Projeto Spring Boot Turma 2020

---
+++ src/main/java/com/aulasprogramacao/controllers/AulasprogramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.aulasprogramacao.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasprogramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasprogramacaoApplication.class, args);
+	}
+
+}
